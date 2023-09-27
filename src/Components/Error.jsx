fa67db0679320acdaa57ce4df01cedf36fd6a69a
Rifat-Shuvo <rhshuvo2812@gmail.com>
2023-09-27 19:21:11 +0600

    A pie chart is implemented and the data ratio of donated and total is sucessfully shown in percentage value in statistics route

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Error = () => {
     return (
         <div className='flex flex-col h-[80vh] justify-center items-center'>
-            {/* <h1 className='text-3xl font-bold mb-6'>Oooopppsssss! An unpexted error occurs.</h1> */}
+            <h1 className='text-3xl font-bold mb-6'>Oooopppsssss! An unpexted error occurs.</h1>
             {/* <Link to={"/"}> <button className='btn btn-error text-white'>Go Home</button> </Link> */}
         </div>
     );
